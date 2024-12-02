235b86e910ed68e844b2171d49b7bc825f353427
zooch-btw <526SDiPalma@frhsd.com>
2024-12-02 11:51:45 -0500

    Lots of things lol

--- script.js
+++ script.js
@@ -6,33 +6,41 @@ userName
 function shakeMagic8Ball() {
   let randomNumber = Math.floor(Math.random() * 8);
   let eightball = "";
-
+  let responseImg;
   let userQuestion = prompt("What is thy question for the MIGHTY 8 ball?");
 
   switch (randomNumber) {
     case 0:
       eightball = "It is certain";
+      responseImg = "it is certain.jpg";
       break;
     case 1:
       eightball = "It is decidely so";
+      responseImg = "It is decidely so.jpg";
       break;
     case 2:
       eightball = "Reply hazy try again";
+      responseImg = "reply hazy try again.jpg";
       break;
     case 3:
       eightball = "Cannot predict now";
+      responseImg = "Cannot predict now.jpg";
       break;
     case 4:
       eightball = "Do not count on it";
+      responseImg = "Do not count on it.jpg";
       break;
     case 5:
       eightball = "My sources say no";
+      responseImg = "My sources say no.jpg";
       break;
     case 6:
       eightball = "Signs point to yes";
+      responseImg = "Signs point to yes.jpg";
       break;
     case 7:
       eightball = "It is certain";
+      responseImg = "it is certain.jpg";
       break;
   }
 
@@ -40,4 +48,5 @@ function shakeMagic8Ball() {
   document.getElementById(
     "response-text"
   ).innerText = `${userName} has asked the Magic 8 Ball ${userQuestion}. The wise and wondrous 8 Ball has replied with ${eightball}`;
+  document.getElementById("response-image").src = responseImg;
 }
